ee1a80a65c657735f6fe7ec0250b6a9414d2d11c
DanielLifshitss <unicornofsky@gmail.com>
2025-09-06 23:07:36 +0300

    started creating item/skins db

--- main-application/database/models/skins.py
+++ main-application/database/models/skins.py
@@ -1,11 +1,29 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
 from database import Base
+from sqlalchemy.dialects.postgresql import JSONB
 
 
-class Skins(Base):
-    __tablename__ = "skins"
+class Item(Base):
+    __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
+    appid = Column(Integer, ForeignKey(), nullable=True)
+    item_asset_id = Column(Integer)
+    item_market_hash_name = Column(String)
+    volume = Column(Integer)
+    item_lowest_price = Column(String)
+    item_median_price = Column(String)
+
+
+class ItemHistory(Base):
+    __tablename__ = "item_history"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    item_price_history = Column(JSONB, nullable=False, default=[])
+    bot_scanner_id = Column(Integer, ForeignKey("bot_scanner.id"), nullable=False)
+    
+    
+
 
 
 
