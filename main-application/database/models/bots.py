b36adaa05c018dd23d087254c118743fcb1eac8d
DanielLifshitss <unicornofsky@gmail.com>
2025-09-08 21:09:56 +0300

    created main application databases schemas

--- main-application/database/models/bots.py
+++ main-application/database/models/bots.py
@@ -1,17 +1,46 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Date
+from sqlalchemy.dialects.postgresql import JSONB
 from database import Base
 
 
 class Bot(Base):
+    
     __tablename__ = "bots"
 
     id = Column(Integer, primary_key=True, index=True)
+    bot_inventory_items = Column(Integer, ForeignKey("inventory.id"), nullable=False)
+    bot_inventory_value = Column(Integer, nullable=False)
 
 
+class Invenotry(Base):
+    __tablename__ = "invenotry"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    tradeable = Column(Boolean, nullable=False)
+    marketable = Column(Boolean, nullable=False)
+    item_price = Column()
+    item_bought_for = Column(Integer, ForeignKey("item_bought.id"))
+    timestamp = Column(Date)
+
+
+class InventoryHistory(Base):
+    __tablename__ = "inventory_history"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    item_id = Column(Integer, ForeignKey('items.id'), nullable=False)
+    item_price = Column()
+    item_bought_for = Column(Integer, ForeignKey("item_bought.id"))
+    item_sold_for = Column(Integer, ForeignKey("item_sold.id"))
+    timestamp = Column(Date)
+    
+    
+    
 class BotScan(Base):
     __tablename__ = "bot_scans"
 
     id = Column(Integer, primary_key=True, index=True)
+    
     
 
 class BotSell(Base):
@@ -19,10 +48,12 @@ class BotSell(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     
+    
 
 class BotBuy(Base):
     __tablename__ = "bot_buys"
 
     id = Column(Integer, primary_key=True, index=True)
     
+
 
--- main-application/database/models/skins.py
+++ main-application/database/models/skins.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean,ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean,ForeignKey, Date
 from database import Base
 from sqlalchemy.dialects.postgresql import JSONB
 
@@ -7,7 +7,7 @@ class Item(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
-    appid = Column(Integer, ForeignKey(), nullable=True)
+    appid = Column(Integer, ForeignKey("games.id"), nullable=False)
     item_asset_id = Column(Integer)
     item_market_hash_name = Column(String)
     volume = Column(Integer)
@@ -15,13 +15,34 @@ class Item(Base):
     item_median_price = Column(String)
 
 
-class ItemHistory(Base):
-    __tablename__ = "item_history"
+class ItemMarketPricesHistory(Base):
+    __tablename__ = "item_market_prices_history"
     
     id = Column(Integer, primary_key=True, index=True)
+    item_id = Column(Integer, ForeignKey("items.id"), nullable=False)
     item_price_history = Column(JSONB, nullable=False, default=[])
     bot_scanner_id = Column(Integer, ForeignKey("bot_scanner.id"), nullable=False)
+    scan_timestamp = Column(Date)
     
+
+class ItemBought(Base):
+    __tablename__ = "item_bought"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    item_id = Column(Integer, ForeignKey("items.id"), nullable=False)
+    item_price_bought = Column(Integer)
+    bot_buyer_id = Column(Integer, ForeignKey("bot_buyer.id"), nullable=False)
+    buy_date_timestamp = Column(Date)
+    
+
+class ItemSold(Base):
+    __tablename__ = "item_sold"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    item_id = Column(Integer, ForeignKey("items.id"), nullable=False)
+    item_price_sold = Column(Integer)
+    bot_seller_id = Column(Integer, ForeignKey("bot_seller.id"), nullable=False)
+    timestamp = Column(Date)
     
 
 
